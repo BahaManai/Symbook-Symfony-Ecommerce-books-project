bc7bd9807b8683a5242d67f04541fa492b37c43d
ghofrane <boutarghofrane@gmail.com>
2025-05-04 18:25:46 +0100

    ajouter des fonction de recherche dans clientcontroller et mise a jourde template --version1

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -12,16 +12,29 @@ use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 final class ClientController extends AbstractController
-{
-    #[Route('/client/livres', name: 'client_livres')]
-    public function all(LivresRepository $livresRepository, PaginatorInterface $paginator, Request $request): Response
+{#[Route('/client/livres', name: 'client_livres')]
+    public function all(Request $request, LivresRepository $livresRepository, PaginatorInterface $paginator): Response
     {
-        $livres = $livresRepository->findAll();
-        $livres = $paginator->paginate($livresRepository->findAll(), $request->query->getInt('page', 1), 12);
+        $filterBy = $request->query->get('filterBy', 'titre');  // Valeur par défaut 'titre'
+        $search = $request->query->get('search', '');  // Valeur par défaut vide
+
+        if ($filterBy && $search) {
+            $livres = $livresRepository->search($filterBy, $search);
+        } else {
+            $livres = $livresRepository->findAll();
+        }
+
+        $livres = $paginator->paginate(
+            $livres,
+            $request->query->getInt('page', 1),
+            12
+        );
+
         return $this->render('Client/livres.html.twig', [
             'livres' => $livres
         ]);
     }
+
 
     #[Route('/client/livre/show/{id}', name: 'client_livre_show')]
     public function show(Livres $livre): Response
